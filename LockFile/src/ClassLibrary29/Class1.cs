cd454c1f9179c261533ec7b4bdb2a853ca954090
David Fowler <davidfowl@gmail.com>
2015-03-10 12:31:27 -0700

    Initial commit

---
+++ LockFile/src/ClassLibrary29/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ClassLibrary29
+{
+    public class Program
+    {
+        public void Main()
+        {
+            Console.WriteLine(Class1.GetHello());
+        }
+    }
+}
